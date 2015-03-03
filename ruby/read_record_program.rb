0163b794b46766d5f4255742ee3822436b27f138
David Socha <David1Socha@gmail.com>
2015-03-03 17:26:54 -0600

    Add class to transform all records

--- ruby/read_record_program.rb
+++ ruby/read_record_program.rb
@@ -3,8 +3,8 @@ require_relative('./read.rb')
 record = ARGV.empty? ? "000000" : ARGV.first
 file_str = File.join(File.dirname(File.expand_path(__FILE__)), "..","data","record_#{record}.dat")
 file = File.new(file_str)
-reader = RecordReader.new(file)
-user, messages = reader.read_record
+reader = RecordReader.new
+user, messages = reader.read_record file
 puts user
 puts
 messages.each do |m|
--- ruby/transform_records.rb
+++ ruby/transform_records.rb
@@ -1,26 +1,55 @@
 require_relative('./read.rb')
-require('yaml')
+require('json')
 
 class RecordTransformer
 
   def initialize
     @reader = RecordReader.new
-  end
-
-  def get_yaml(messages)
-    yaml = messages.map{|msg| YAML::dump(msg)[2..-1]}.join
+    @parent_dir = File.join(File.dirname(File.expand_path(__FILE__)), "..")
+    @messages_written = 0
+    @users_written = 0
   end
 
   def store_records(num_records)
+    t1 = Time.now
     num_records.times do |n|
       record = n.to_s.rjust(6, '0')
-      file_str = File.join(File.dirname(File.expand_path(__FILE__)), "..","data","record_#{record}.dat")
+      file_str = File.join(@parent_dir,"data","record_#{record}.dat")
       file = File.new(file_str)
       store_record file
     end
+    t2 = Time.now
+    delta = t2 - t1
   end
 
   def store_record(file)
-    user, messages = @reader.read_record
+    user, messages = @reader.read_record file
+
+    store_user(file, user)
+
+    store_messages(file, messages)
   end
+
+  def store_user(file, user)
+    user_json = user.to_json
+    puts user_json
+    user_path = File.join(@parent_dir, "customized_data", "user_#{@users_written.to_s.rjust(6, "0")}.dat")
+    File.open(user_path, 'w+') {|f| f.write user_json}
+    @users_written += 1
+  end
+
+  def store_messages(file, messages)
+    messages.each do |msg|
+      msg_json = msg.to_json
+      puts msg_json
+      msg_path = File.join(@parent_dir, "customized_data", "message_#{@messages_written.to_s.rjust(6, "0")}.dat")
+      File.open(msg_path, 'w+') {|f| f.write msg_json}
+      @messages_written += 1
+    end
+  end
+
 end
+
+t = RecordTransformer.new
+delta = t.store_records(2000)
+puts "took #{delta} seconds"
