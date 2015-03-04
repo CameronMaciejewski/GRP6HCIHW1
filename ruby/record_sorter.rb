1a32d9b6c7dbf7086ada4f232c42f35b35f419c4
David Socha <David1Socha@gmail.com>
2015-03-03 20:50:19 -0600

    Add ModelLoader

---
+++ ruby/record_sorter.rb
@@ -0,0 +1,46 @@
+require('json')
+require_relative('./user_loader.rb')
+
+class RecordSorter
+
+  def initialize
+    @user_loader = UserLoader.new
+    @parent_dir = File.join(File.dirname(File.expand_path(__FILE__)), "..")
+    @output_dir = File.join(@parent_dir, "customized_data")
+  end
+
+  def sort(model_to_sort, sort_row)
+    if model_to_sort == "user" then
+      sort_users sort_row
+    else
+      sort_messages sort_row
+    end
+  end
+
+  def sort_messages(sort_row)
+    messages = @user_loader.load_messages
+    sorted = messages.sort_by &sort_row
+    sorted.each_with_index do |m, i|
+      m.id = i
+      out_file = File.join(@output_dir, "message_#{i.to_s.rjust(6,"0")}.dat")
+      msg_json = m.to_json
+      puts msg_json
+      File.open(out_file, 'w+') {|f| f.write msg_json}
+    end
+  end
+
+  def sort_users(sort_row)
+    old_new_mapping = Hash.new #TODO
+    users = @user_loader.load_users
+    sorted = users.sort_by &sort_row
+    sorted.each_with_index do |u, i|
+      old_new_mapping[u.id] = i
+      u.id = i
+      out_file = File.join(@output_dir, "user_#{i.to_s.rjust(6,"0")}.dat")
+      user_json = u.to_json
+      puts user_json
+      File.open(out_file, 'w+') {|f| f.write user_json}
+    end
+  end
+
+end
--- ruby/sort_records_program.rb
+++ ruby/sort_records_program.rb
@@ -1,2 +1,8 @@
-require('json')
-#todo
+require_relative('./record_sorter.rb')
+
+t1 = Time.now
+record_sorter = RecordSorter.new
+record_sorter.sort(ARGV[0], ARGV[1].to_sym)
+t2 = Time.now
+delta = t2 - t1
+puts "sort took #{delta} seconds"
