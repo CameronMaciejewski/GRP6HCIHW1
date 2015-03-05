4245415418af8dcd3d0e19fc801d77729dd3699b
Cameron Maciejewski <cmaciejew@yahoo.com>
2015-03-04 21:38:29 -0600

    Updated Program.cs

--- BPlusTree/BPlusTree/Program.cs
+++ BPlusTree/BPlusTree/Program.cs
@@ -4,22 +4,50 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using Newtonsoft.Json;
 
 namespace BPlusTree
 {
     class Program
     {
+        static User[] usersById;
+        static User[] usersByLocation;
+        static Message[] messagesById;
         static void Main(string[] args)
         {
-            string directory;
-            do{
-                directory = getDirectory();
-            } while(directory == "-1");
+            string messagesDirectory;
+            string usersByIdDirectory;
+            string usersByLocationDirectory;
 
-            
+            do
+            {
+                messagesDirectory = getDirectory("messages", "time");
+            } while (messagesDirectory == "-1");
+
+            Tree<int> messagesTree10 = new Tree<int>(10);
+            Tree<int> messagesTree200 = new Tree<int>(200);
+
+            do
+            {
+                usersByIdDirectory = getDirectory("users", "Id");
+            } while (usersByIdDirectory == "-1");
+
+            Tree<int> usersByIdTree10 = generateUserByIdTree(usersByIdDirectory, 10);
+            Tree<int> usersByIdTree200 = generateUserByIdTree(usersByIdDirectory, 200);
+
+            do
+            {
+                usersByLocationDirectory = getDirectory("users", "location");
+            } while (usersByLocationDirectory == "-1");
+
+            Tree<string> usersByLocationTree10 = generateUserByLocationTree(usersByLocationDirectory, 10);
+            Tree<string> usersByLocationTree200 = generateUserByLocationTree(usersByLocationDirectory, 200);
+
+
         }
-        public static string getDirectory(){
-            Console.WriteLine("Please enter the directory of the files you wish to put into a Bplus tree (ex: C:/Users/John/Documents/Files): ");
+        public static string getDirectory(string type, string sortedBy)
+        {
+            Console.WriteLine("Please enter the directory of the " + type + " you wish to put into a Bplus Tree sorted by " + sortedBy + " (ex: C:/Users/John/Documents/Files): ");
             string filePath = Console.ReadLine();
             if (Directory.Exists(filePath) && Directory.GetFiles(filePath).Length != 0)
             {
@@ -35,6 +63,93 @@ namespace BPlusTree
                 Console.WriteLine("There are no files in the directory " + filePath);
                 return "-1";
             }
+        }
+
+        public static Tree<string> generateUserByLocationTree(string directory, int fanOut)
+        {
+            IEnumerable<string> fileNames = Directory.EnumerateFiles(directory);
+            string[] fileNamesArray = fileNames.ToArray<string>();
+            List<User> users = new List<User>();
+            string[] locations = new string[fileNamesArray.Length];
+            for (int i = 0; i < fileNamesArray.Count(); i++)
+            {
+                string userText = File.ReadAllText(fileNamesArray[i]);
+                users.Add(JsonConvert.DeserializeObject<User>(userText));
+                locations[i] = users[i].state;
+            }
+            if (usersByLocation.Length == 0)
+            {
+                usersByLocation = users.ToArray();
+            }
+
+            Tree<string> tree = new Tree<string>(fanOut);
+            tree.createTree(locations);
+            return tree;
+        }
+
+        public static Tree<int> generateUserByIdTree(string directory, int fanOut)
+        {
+            IEnumerable<string> fileNames = Directory.EnumerateFiles(directory);
+            string[] fileNamesArray = fileNames.ToArray<string>();
+            List<User> users = new List<User>();
+            int[] ids = new int[fileNamesArray.Length];
+            for (int i = 0; i < fileNamesArray.Count(); i++)
+            {
+                string userText = File.ReadAllText(fileNamesArray[i]);
+                users.Add(JsonConvert.DeserializeObject<User>(userText));
+                ids[i] = users[i].id;
+            }
+            if (usersById.Length == 0)
+            {
+                usersById = users.ToArray();
+            }
+
+            Tree<int> tree = new Tree<int>(fanOut);
+            tree.createTree(ids);
+            return tree;
+        }
+
+        public static Tree<int> generateMessagesTree(string directory, int fanOut)
+        {
+            IEnumerable<string> fileNames = Directory.EnumerateFiles(directory);
+            string[] fileNamesArray = fileNames.ToArray<string>();
+            List<Message> messages = new List<Message>();
+            int[] ids = new int[fileNamesArray.Length];
+            for (int i = 0; i < fileNamesArray.Count(); i++)
+            {
+                string userText = File.ReadAllText(fileNamesArray[i]);
+                messages.Add(JsonConvert.DeserializeObject<Message>(userText));
+                ids[i] = messages[i].id;
+            }
+            if (messagesById.Length == 0)
+            {
+                messagesById = messages.ToArray();
+            }
+
+            Tree<int> tree = new Tree<int>(fanOut);
+            tree.createTree(ids);
+            return tree;
+
+        }
+
+        public static List<User> getUsersFromNebraska()
+        {
+
+        }
+
+        public static List<User> getUsersWhoSentMessagesFromEightToNine()
+        {
+
+        }
+
+        public static List<User> getUsersWhoSentMessagesFromEightToNineFromNebraska()
+        {
+
+        }
+
+        public static User getUserWhoSentMostMessagesFromEightToNineInNebraska()
+        {
+
         }
     }
 }
