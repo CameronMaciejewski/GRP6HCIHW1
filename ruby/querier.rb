9836a3d0ec90d71c25b836d780530e426dae9ec4
David Socha <David1Socha@gmail.com>
2015-03-04 19:48:27 -0600

    Add queries for last 2

--- ruby/querier.rb
+++ ruby/querier.rb
@@ -16,4 +16,32 @@ class Querier
     num_users_messaging_in_range = messages_in_range.uniq { |m| m.user_id }.size
   end
 
+  def get_users_messaging_during(min_hour, max_hour)
+    messages = @model_loader.load_messages
+    users = @model_loader.load_users
+    messages_in_range = messages.select { |m| m.hour >= min_hour && ( m.hour < max_hour || ( m.hour == max_hour && m.minute == 0 ) ) }
+    uniq_user_ids_in_range = messages_in_range.map {|m| m.user_id}.uniq
+    users_in_range = users.select { |u| uniq_user_ids_in_range.include? u.id}
+  end
+
+  def get_user_max_messages_during(state, min_hour, max_hour)
+    messages = @model_loader.load_messages
+    users = @model_loader.load_users
+    messages_in_range = messages.select { |m| m.hour >= min_hour && ( m.hour < max_hour || ( m.hour == max_hour && m.minute == 0 ) ) }
+    state_users = get_all_from_state state
+    user_message_counts = Array.new(state_users.size, 0)
+    state_users.each_with_index do |u, i|
+      if messages_in_range.any? { |m| m.user_id == u.id}
+        user_message_counts[i] += 1
+      end
+    end
+    index_max_user = user_message_counts.each_with_index.max[1]
+    max_user = state_users[index_max_user]
+  end
+
+  def get_users_from_state_messaging_during(state, min_hour, max_hour)
+    users_in_range = get_users_messaging_during(min_hour, max_hour)
+    users_in_range_and_state = users_in_range.select { |u| u.state == state}
+  end
+
 end
--- ruby/query_program.rb
+++ ruby/query_program.rb
@@ -9,8 +9,13 @@ if query == "a"
 elsif query == "b"
   num_users_messaging_at_8 = querier.get_num_users_messaging_during(8, 9)
   puts "Number of users messaging from 8am to 9am inclusive: #{num_users_messaging_at_8}"
+elsif query == "c"
+  num_nebraskans_messaging_at_8 = querier.get_users_from_state_messaging_during("Nebraska", 8, 9).size
+  puts "Number of Nebraskans messaging from 8am to 9am inclusive: #{num_nebraskans_messaging_at_8}"
+else
+  max_nebraskan_at_8 = querier.get_user_max_messages_during("Nebraska", 8, 9)
+  puts "ID of Nebraskan sending most messages from 8am to 9am: #{max_nebraskan_at_8.id}"
 end
-
 
 t2 = Time.now
 delta = t2 - t1
